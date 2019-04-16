408d46c7c56c7817eb33af1ec9d4108df664139d
Jaystar2323 <47769047+Jaystar2323@users.noreply.github.com>
2019-04-16 08:45:09 -0700

    Camera zooming

--- TowerDefenseSpring/Assets/Scripts/CameraControler.cs
+++ TowerDefenseSpring/Assets/Scripts/CameraControler.cs
@@ -10,6 +10,14 @@ public class CameraControler : MonoBehaviour {
     public float minY = 10f;
     public float maxY = 80f;
 
+    public float minVertical = -90f;
+    public float maxVertical = 0f;
+    public float minHorizontal = -75;
+    public float maxHorizontal = 75;
+
+    public float minRotation = .395f;
+    public float maxRotation = .546f;
+
     private bool doMovement = true;
 
 
@@ -24,18 +32,35 @@ public class CameraControler : MonoBehaviour {
         }
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
+            if(transform.position.z >= maxVertical)
+            { 
+                return;
+            }
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
         {
+            if (transform.position.z <= minVertical)
+            {
+                return;
+            }
             transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
         {
+            
+            if (transform.position.x <= minVertical)
+            {
+                return;
+            }
             transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
+            if (transform.position.x >= maxHorizontal)
+            {
+                return;
+            }
             transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
         }
 
@@ -43,6 +68,14 @@ public class CameraControler : MonoBehaviour {
         //Debug.Log(scroll);
 
         Vector3 pos = transform.position;
+
+        Quaternion rotation = transform.rotation;
+        rotation.x -= scroll * 7 * Time.deltaTime;
+        Debug.Log(rotation.x);
+        rotation.x = Mathf.Clamp(rotation.x, minRotation, maxRotation);
+        Debug.Log(rotation.x);
+        transform.rotation = rotation;
+
         pos.y -= scroll * 500 * scrollSpeed * Time.deltaTime;
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
         transform.position = pos;
